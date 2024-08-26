7b29918d21c091f15c4e6ec2a91833890e931457
smipark <smi.parkdev@gmail.com>
2024-08-26 15:54:07 +0900

    ㅅㄷㄴㅅ22

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1724655008',resources: [
+    cacheName: 'chirpy-1724655155',resources: [
       '/https://smipark.kr/assets/css/jekyll-theme-chirpy.css',
       '/https://smipark.kr/',
       
